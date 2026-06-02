5196d968f367b1c603f554b5eb4ab0b105c681c3
Cassio Souza <cassio.souza@ancar.local>
2026-06-02 17:58:26 -0300

    fix: correção de bug

--- src/controllers/health.controller.ts
+++ src/controllers/health.controller.ts
@@ -1,31 +1,10 @@
 import { Request, Response } from 'express';
-import prisma from '../database/prisma.js';
-import { rabbitMQ } from '../config/rabbitmql.config.js';
 
 export class HealthController {
-
   async check(req: Request, res: Response) {
-
-    try {
-
-      await prisma.$queryRaw`SELECT 1`;
-
-      return res.status(200).json({
-        status: 'UP',
-        database: 'UP',
-        rabbitmq: rabbitMQ.isConnected
-          ? 'UP'
-          : 'DOWN',
-        timestamp: new Date().toISOString()
-      });
-
-    } catch {
-
-      return res.status(503).json({
-        status: 'DOWN',
-        timestamp: new Date().toISOString()
-      });
-
-    }
+    return res.status(200).json({
+      status: 'UP',
+      timestamp: new Date().toISOString()
+    });
   }
 }
