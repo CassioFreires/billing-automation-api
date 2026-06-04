fb351db9d6faac53ec5af7a21672625ff68d471c
Cassio Souza <cassio.souza@ancar.local>
2026-06-04 09:02:35 -0300

    fix: correção do health dockerfile

--- dist/src/controllers/health.controller.js
+++ dist/src/controllers/health.controller.js
@@ -1,23 +1,8 @@
-import prisma from '../database/prisma.js';
-import { rabbitMQ } from '../config/rabbitmql.config.js';
 export class HealthController {
     async check(req, res) {
-        try {
-            await prisma.$queryRaw `SELECT 1`;
-            return res.status(200).json({
-                status: 'UP',
-                database: 'UP',
-                rabbitmq: rabbitMQ.isConnected
-                    ? 'UP'
-                    : 'DOWN',
-                timestamp: new Date().toISOString()
-            });
-        }
-        catch {
-            return res.status(503).json({
-                status: 'DOWN',
-                timestamp: new Date().toISOString()
-            });
-        }
+        return res.status(200).json({
+            status: 'UP',
+            timestamp: new Date().toISOString()
+        });
     }
 }
